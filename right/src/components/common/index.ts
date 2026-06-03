8908634fbff696b01ecea180e8762c20bb8b5d80
栗田孟 <akito14140099@gmail.com>
2026-06-03 15:20:37 +0900

    環境設定

--- right/src/components/common/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Header } from './Header';
-export { default as Footer } from './Footer';
-export { default as Button } from './Button';
-export { default as Card } from './Card';
-export { default as BlobBackground } from './BlobBackground';
--- right/src/components/sections/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Hero } from './Hero';
-export { default as About } from './About';
-export { default as Culture } from './Culture';
-export { default as Jobs } from './Jobs';
-export { default as Environment } from './Environment';
-export { default as NewGraduate } from './NewGraduate';
-export { default as Blog } from './Blog';
